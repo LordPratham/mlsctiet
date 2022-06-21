9ce861c131edf70e7d4c963266e23e9ed2bedef2
Nimish Medatwal <medatwalnimish@gmail.com>
2022-06-21 16:06:57 +0530

    added routing

--- src/Pages/OurTeam/Components/Navbar.js
+++ src/Pages/OurTeam/Components/Navbar.js
@@ -1,26 +1,55 @@
 import React from 'react'
 import styles from './../Style/ourteam.module.css'
-import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import { Link, useMatch, useResolvedPath, BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Secretaries from './Secretaries'
+import Heads from './Heads'
+import Core from './Core'
+import Executives from './Executives'
 
 const Navbar = () => {
   return (
-    <nav>
+
+    <Router>
+      <nav>
         <ul className={styles.navbar}>
           <li className={styles.firstcomp}>
-            <a href="#"> <CustomLink to="/Secretaries">Pricing</CustomLink></a>
+            <Link to="/Secretaries">Secretaries</Link>
           </li>
           <li>
-            <a href="#">Heads</a>
+            <Link to="/Heads">Heads</Link>
           </li>
           <li>
-            <a href="#">Core</a>
+            <Link to="/Core">Core</Link>
           </li>
           <li>
-            <a href="#">Executives</a>
+            <Link to="/Executives">Executives</Link>
           </li>
         </ul>
+
+      
       </nav>
+
+      <Routes>
+            <Route path="/Secretaries" element={<Secretaries />}/>
+            <Route path="/Heads" element={<Heads />}/>
+            <Route path="/Core" element={<Core />}/>
+            <Route path="/Executives" element={<Executives />}/>
+
+      </Routes> 
+    </Router> 
   )
+  function CustomLink({ to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+  
+    return (
+      <li className={isActive ? "active" : ""}>
+        <Link to={to} {...props}>
+          {children}
+        </Link>
+      </li>
+    )
+  }
 }
 
 export default Navbar
